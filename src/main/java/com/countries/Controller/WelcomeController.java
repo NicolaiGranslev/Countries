6a726774880ea6af41c3aef212e312153a250693
megamindEK <granslev.dk@gmail.com>
2026-03-10 14:31:38 +0100

    Update 1

---
+++ src/main/java/com/countries/Controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.countries.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    public class WelcomeController {
+
+        @GetMapping("/")
+        public String welcome() {
+            return "Welcome to the countries website!";
+        }
+    }
